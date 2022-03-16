d18f8e3135c60c87c9e9c90a15c21c575989453d
Larissa Amaral <larissa95.amaral@gmail.com>
2022-03-15 22:52:42 -0300

    configures key access from outside classpath

--- bin/gen-non-prod-key.sh
+++ bin/gen-non-prod-key.sh
@@ -1,20 +1,21 @@
-CLIENT_KEYSTORE_DIR=../client/src/main/resources
-SERVER_KEYSTORE_DIR=../server/src/main/resources
-CLIENT_KEYSTORE=$CLIENT_KEYSTORE_DIR/client-nonprod.jks
-SERVER_KEYSTORE=$SERVER_KEYSTORE_DIR/server-nonprod.jks
-JAVA_CA_CERTS=$JAVA_HOME/jre/lib/security/cacerts
+CLIENT_KEYSTORE=client-keystore
+SERVER_KEYSTORE=server-keystore
 
 CLIENT_KEY_ALIAS=client
 SERVER_KEY_ALIAS=server
 
 # Generate a client and server RSA 2048 key pair
-keytool -genkeypair -alias $CLIENT_KEY_ALIAS -keyalg RSA -keysize 2048 -dname "CN=Client,OU=Client,O=PlumStep,L=San Francisco,S=CA,C=U" -keypass changeme -keystore $CLIENT_KEYSTORE -storepass changeme
-keytool -genkeypair -alias $SERVER_KEY_ALIAS -keyalg RSA -keysize 2048 -dname "CN=Server,OU=Server,O=PlumStep,L=San Francisco,S=CA,C=U" -keypass changeme -keystore $SERVER_KEYSTORE -storepass changeme
+keytool -genkeypair -alias $CLIENT_KEY_ALIAS -keyalg RSA -keysize 2048 -dname "CN=Client,OU=Client,O=PlumStep,L=San Francisco,S=CA,C=U" -keypass 123456 -keystore $CLIENT_KEYSTORE.jks -storepass 123456
+keytool -genkeypair -alias $SERVER_KEY_ALIAS -keyalg RSA -keysize 2048 -dname "CN=Server,OU=Server,O=PlumStep,L=San Francisco,S=CA,C=U" -keypass 123456 -keystore $SERVER_KEYSTORE.jks -storepass 123456
 
 # Export public certificates for both the client and server
-keytool -exportcert -alias $CLIENT_KEY_ALIAS -file $CLIENT_KEY_ALIAS-public.cer -keystore $CLIENT_KEYSTORE -storepass changeme
-keytool -exportcert -alias $SERVER_KEY_ALIAS -file $SERVER_KEY_ALIAS-public.cer -keystore $SERVER_KEYSTORE -storepass changeme
+keytool -exportcert -alias $CLIENT_KEY_ALIAS -file $CLIENT_KEY_ALIAS-public.cer -keystore $CLIENT_KEYSTORE.jks -storepass 123456
+keytool -exportcert -alias $SERVER_KEY_ALIAS -file $SERVER_KEY_ALIAS-public.cer -keystore $SERVER_KEYSTORE.jks -storepass 123456
 
 # Import the client and server public certificates into each others keystore
-keytool -importcert -keystore $CLIENT_KEYSTORE -alias $SERVER_KEY_ALIAS-public-cert -file $SERVER_KEY_ALIAS-public.cer -storepass changeme -noprompt
-keytool -importcert -keystore $SERVER_KEYSTORE -alias $CLIENT_KEY_ALIAS-public-cert -file $CLIENT_KEY_ALIAS-public.cer -storepass changeme -noprompt
+keytool -importcert -keystore $CLIENT_KEYSTORE.jks -alias $SERVER_KEY_ALIAS-public-cert -file $SERVER_KEY_ALIAS-public.cer -storepass 123456 -noprompt
+keytool -importcert -keystore $SERVER_KEYSTORE.jks -alias $CLIENT_KEY_ALIAS-public-cert -file $CLIENT_KEY_ALIAS-public.cer -storepass 123456 -noprompt
+
+# Convert .jks to .p12
+keytool -importkeystore -srckeystore $CLIENT_KEYSTORE.jks -destkeystore $CLIENT_KEYSTORE.p12 -srcstoretype JKS -deststoretype PKCS12 -srcstorepass 123456 -deststorepass 123456
+keytool -importkeystore -srckeystore $SERVER_KEYSTORE.jks -destkeystore $SERVER_KEYSTORE.p12 -srcstoretype JKS -deststoretype PKCS12 -srcstorepass 123456 -deststorepass 123456
--- client/src/main/java/com/plumstep/ClientApplication.java
+++ client/src/main/java/com/plumstep/ClientApplication.java
@@ -1,17 +1,22 @@
 package com.plumstep;
 
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Paths;
+
 import javax.net.ssl.SSLContext;
 
 import org.apache.http.client.HttpClient;
 import org.apache.http.conn.ssl.NoopHostnameVerifier;
 import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
 import org.apache.http.impl.client.HttpClients;
+import org.apache.http.ssl.SSLContextBuilder;
 import org.apache.http.ssl.SSLContexts;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.core.io.Resource;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
@@ -27,16 +32,16 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @SpringBootApplication
 @EnableSwagger2
 public class ClientApplication {
-    @Value("${server.ssl.trust-store-password}")
-    private String trustStorePassword;
-    @Value("${server.ssl.trust-store}")
-    private Resource trustStore;
-    @Value("${server.ssl.key-store-password}")
-    private String keyStorePassword;
-    @Value("${server.ssl.key-password}")
-    private String keyPassword;
-    @Value("${server.ssl.key-store}")
-    private Resource keyStore;
+	@Value("${server.ssl.trust-store-password}")
+	private String trustStorePassword;
+	@Value("${server.ssl.trust-store:}")
+	private String trustResource;
+	@Value("${server.ssl.key-store-password}")
+	private String keyStorePassword;
+	@Value("${server.ssl.key-password}")
+	private String keyPassword;
+	@Value("${server.ssl.key-store:}")
+	private String keyStore;
 
     public static void main(String[] args) {
         SpringApplication.run(ClientApplication.class, args);
@@ -70,13 +75,31 @@ public class ClientApplication {
     }
 
     private HttpClient httpClient() throws Exception {
-        // Load our keystore and truststore containing certificates that we trust.
-        SSLContext sslcontext =
-                SSLContexts.custom().loadTrustMaterial(trustStore.getURL(), trustStorePassword.toCharArray())
-                        .loadKeyMaterial(keyStore.getURL(), keyStorePassword.toCharArray(),
-                                keyPassword.toCharArray()).build();
-        SSLConnectionSocketFactory sslConnectionSocketFactory =
-                new SSLConnectionSocketFactory(sslcontext, new NoopHostnameVerifier());
-        return HttpClients.custom().setSSLSocketFactory(sslConnectionSocketFactory).build();
-    }
+		// Load our keystore and truststore containing certificates that we trust.
+		SSLContextBuilder builder = SSLContexts.custom();
+		if(isExternalResource(trustResource)) {
+			builder.loadTrustMaterial(Paths.get(trustResource).toFile(), trustStorePassword.toCharArray());
+		} else {
+			builder.loadTrustMaterial(getInternalResourceUrl(trustResource), trustStorePassword.toCharArray());
+		}
+		
+		if(isExternalResource(keyStore)) {
+			builder.loadKeyMaterial(Paths.get(keyStore).toFile(), keyStorePassword.toCharArray(), keyPassword.toCharArray()).build();
+		} else {
+			builder.loadKeyMaterial(getInternalResourceUrl(keyStore), keyStorePassword.toCharArray(), keyPassword.toCharArray()).build();
+		}
+		
+		SSLContext sslcontext = builder.build();
+		SSLConnectionSocketFactory sslConnectionSocketFactory = new SSLConnectionSocketFactory(sslcontext,
+				new NoopHostnameVerifier());
+		return HttpClients.custom().setSSLSocketFactory(sslConnectionSocketFactory).build();
+	}
+	
+	private boolean isExternalResource(String path) {
+		return !path.contains("classpath:");
+	}
+	
+	private URL getInternalResourceUrl(String path) throws IOException {
+		return new ClassPathResource(trustResource.replaceAll("classpath:", "")).getURL();
+	}
 }
